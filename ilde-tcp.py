1d5843c8c0288b5bcb884872803d29d0eabf1632
Болховитинов Дмитрий <bolhovitinov_d@goldapple.ru>
2017-08-15 17:19:25 +0500

    fix socket 0.0.0.0

--- ilde-tcp.py
+++ ilde-tcp.py
@@ -27,7 +27,7 @@ cpool = Utils.getCollectionPool(config, dbprefix)
 
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = ('0.0.0.0', 8801)
+server_address = ("", 8801)
 print >>sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 
