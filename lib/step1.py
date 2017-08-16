4f4551fd49164c330f50fd9bce6fb6fed9116560
Болховитинов Дмитрий <bolhovitinov_d@goldapple.ru>
2017-08-16 16:39:30 +0500

    fix import ssl

--- lib/step1.py
+++ lib/step1.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import ssl
 import json
 import socket
 import urllib2
--- lib/step25.py
+++ lib/step25.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import ssl
 import json
 import socket
 import urllib2
